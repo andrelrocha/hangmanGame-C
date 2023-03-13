bad681fceb6b3783d9baf653bb8313f480c15aab
andrelrocha <andrerocha0911@gmail.com>
2023-03-13 18:10:49 -0300

    usando string.h

--- draftCoding/forca.C
+++ draftCoding/forca.C
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 {
-    char secretWord[10] = "Apple";
-    char secretWord2[20];
-    sprintf(secretWord2, "Pineapple");
-    printf("%s\n", secretWord2);
-    printf("%s", secretWord);
+    char secretWord[20];
+    sprintf(secretWord, "pineapple");
+    
+    int acertou = 0;
+    int enforcou = 0;
+
+    do {
+        
+        
+        char chute;
+        scanf("%c", &chute);
+
+        for (int i = 0; i < strlen(secretWord); i++) {
+            if (secretWord[i] == chute) {
+                printf("A posicao %d tem essa letra.\n", i);
+            }
+        }
+    } while (!acertou && !enforcou) {
+
+    }
+
 };
