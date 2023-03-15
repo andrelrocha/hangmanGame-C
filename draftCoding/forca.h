bf75925901c469f62339e3648524f2b73606245a
andrelrocha <andrerocha0911@gmail.com>
2023-03-15 17:45:10 -0300

    add arquivo .h com as declarações globais do programa

---
+++ draftCoding/forca.h
@@ -0,0 +1,12 @@
+void abertura();
+void chuta();
+int jaChutou (char letra);
+void desenhaForca();
+void escolhePalavra();
+int acertou();
+int enforcou();
+
+//variaveis globais
+char secretWord[20];
+char chutes[26];
+int chutesDados = 0;
